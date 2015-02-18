baea5446a2ffcd6497b6397bf678947efb2221d8
Rocky Randalls <rrandalls@gmail.com>
2015-02-18 17:45:16 -0500

    Add test surveys redirect

--- test/controllers/surveys_controller_test.rb
+++ test/controllers/surveys_controller_test.rb
@@ -1,4 +1,8 @@
 require 'test_helper'
 
 class SurveysControllerTest < ActionController::TestCase
+  test "should get go_to_time" do
+    get :index
+    assert_redirected_to sessions_log_in_path
+  end
 end
