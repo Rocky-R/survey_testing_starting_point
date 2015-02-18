49a43637c9cab87eea02976000ccbc689fe25c47
Rocky Randalls <rrandalls@gmail.com>
2015-02-18 16:40:45 -0500

    Add test Submissions#index

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -1,4 +1,10 @@
 require 'test_helper'
 
 class SubmissionsControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:submissions)
+  end
 end
